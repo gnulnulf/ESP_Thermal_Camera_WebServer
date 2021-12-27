79c1380de3d04e4814659b5c98199fb061ccbd97
Arco van Geest <arco@appeltaart.mine.nu>
2021-12-27 10:46:35 +0100

    html in spiffs

--- ESP_Thermal_Camera_WebServer.ino
+++ ESP_Thermal_Camera_WebServer.ino
@@ -8,6 +8,11 @@
 // GitHub: https://github.com/Samox1/ESP_Thermal_Camera_WebServer                                         //
 //--------------------------------------------------------------------------------------------------------//
 
+
+// test html application/json 
+// cd ESP_Thermal_Camera_WebServer/data
+// $ python -m http.server 8000
+
 #include <SPI.h>
 #include <Wire.h>
 #include <WiFi.h>
@@ -19,13 +24,14 @@
 #include "MLX90640_I2C_Driver.h"
 #include "MLX90640_API.h"
 
-
 const byte MLX90640_address = 0x33; //Default 7-bit unshifted address of the MLX90640
 
 #define TA_SHIFT 8 //Default shift for MLX90640 in open air
 
 float mlx90640To[768];
 paramsMLX90640 mlx90640;
+
+volatile boolean dataready;
 
 // You can use any (4 or) 5 pins
 #define sclk 18
@@ -48,7 +54,7 @@ paramsMLX90640 mlx90640;
 #define PART_BOUNDARY "123456789000000000000987654321"
 
 //the colors we will be using
-const uint16_t camColors[] = {0x480F,
+/*const uint16_t camColors[] = {0x480F,
                               0x400F, 0x400F, 0x400F, 0x4010, 0x3810, 0x3810, 0x3810, 0x3810, 0x3010, 0x3010,
                               0x3010, 0x2810, 0x2810, 0x2810, 0x2810, 0x2010, 0x2010, 0x2010, 0x1810, 0x1810,
                               0x1811, 0x1811, 0x1011, 0x1011, 0x1011, 0x0811, 0x0811, 0x0811, 0x0011, 0x0011,
@@ -76,14 +82,49 @@ const uint16_t camColors[] = {0x480F,
                               0xF1E0, 0xF1C0, 0xF1A0, 0xF180, 0xF160, 0xF140, 0xF100, 0xF0E0, 0xF0C0, 0xF0A0,
                               0xF080, 0xF060, 0xF040, 0xF020, 0xF800,
                              };
+*/
+const uint16_t camColors[] = {0x480F,
+0x400F,0x400F,0x400F,0x4010,0x3810,0x3810,0x3810,0x3810,0x3010,0x3010,
+0x3010,0x2810,0x2810,0x2810,0x2810,0x2010,0x2010,0x2010,0x1810,0x1810,
+0x1811,0x1811,0x1011,0x1011,0x1011,0x0811,0x0811,0x0811,0x0011,0x0011,
+0x0011,0x0011,0x0011,0x0031,0x0031,0x0051,0x0072,0x0072,0x0092,0x00B2,
+0x00B2,0x00D2,0x00F2,0x00F2,0x0112,0x0132,0x0152,0x0152,0x0172,0x0192,
+0x0192,0x01B2,0x01D2,0x01F3,0x01F3,0x0213,0x0233,0x0253,0x0253,0x0273,
+0x0293,0x02B3,0x02D3,0x02D3,0x02F3,0x0313,0x0333,0x0333,0x0353,0x0373,
+0x0394,0x03B4,0x03D4,0x03D4,0x03F4,0x0414,0x0434,0x0454,0x0474,0x0474,
+0x0494,0x04B4,0x04D4,0x04F4,0x0514,0x0534,0x0534,0x0554,0x0554,0x0574,
+0x0574,0x0573,0x0573,0x0573,0x0572,0x0572,0x0572,0x0571,0x0591,0x0591,
+0x0590,0x0590,0x058F,0x058F,0x058F,0x058E,0x05AE,0x05AE,0x05AD,0x05AD,
+0x05AD,0x05AC,0x05AC,0x05AB,0x05CB,0x05CB,0x05CA,0x05CA,0x05CA,0x05C9,
+0x05C9,0x05C8,0x05E8,0x05E8,0x05E7,0x05E7,0x05E6,0x05E6,0x05E6,0x05E5,
+0x05E5,0x0604,0x0604,0x0604,0x0603,0x0603,0x0602,0x0602,0x0601,0x0621,
+0x0621,0x0620,0x0620,0x0620,0x0620,0x0E20,0x0E20,0x0E40,0x1640,0x1640,
+0x1E40,0x1E40,0x2640,0x2640,0x2E40,0x2E60,0x3660,0x3660,0x3E60,0x3E60,
+0x3E60,0x4660,0x4660,0x4E60,0x4E80,0x5680,0x5680,0x5E80,0x5E80,0x6680,
+0x6680,0x6E80,0x6EA0,0x76A0,0x76A0,0x7EA0,0x7EA0,0x86A0,0x86A0,0x8EA0,
+0x8EC0,0x96C0,0x96C0,0x9EC0,0x9EC0,0xA6C0,0xAEC0,0xAEC0,0xB6E0,0xB6E0,
+0xBEE0,0xBEE0,0xC6E0,0xC6E0,0xCEE0,0xCEE0,0xD6E0,0xD700,0xDF00,0xDEE0,
+0xDEC0,0xDEA0,0xDE80,0xDE80,0xE660,0xE640,0xE620,0xE600,0xE5E0,0xE5C0,
+0xE5A0,0xE580,0xE560,0xE540,0xE520,0xE500,0xE4E0,0xE4C0,0xE4A0,0xE480,
+0xE460,0xEC40,0xEC20,0xEC00,0xEBE0,0xEBC0,0xEBA0,0xEB80,0xEB60,0xEB40,
+0xEB20,0xEB00,0xEAE0,0xEAC0,0xEAA0,0xEA80,0xEA60,0xEA40,0xF220,0xF200,
+0xF1E0,0xF1C0,0xF1A0,0xF180,0xF160,0xF140,0xF100,0xF0E0,0xF0C0,0xF0A0,
+0xF080,0xF060,0xF040,0xF020,0xF800,};
 
 //WebServer server(80);
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
 //Enter your SSID and PASSWORD
-const char* ssid = "ESP";
-const char* password = "camera";
+const char *apssid = "MLX90640";
+const char *appassword = "camera";
+
+
+
+void notFound(AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
+}
+
 
 float p = 3.1415926;
 Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, mosi, sclk, rst);
@@ -107,103 +148,7 @@ String getMinTemp(){
 }
 
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <style>
-    html {
-     font-family: Arial;
-     display: inline-block;
-     margin: 0px auto;
-     text-align: center;
-    }
-    h2 { font-size: 3.0rem; }
-    p { font-size: 3.0rem; }
-    .units { font-size: 1.2rem; }
-    .dht-labels{
-      font-size: 1.5rem;
-      vertical-align:middle;
-      padding-bottom: 15px;
-    }
-    img { 
-      display: inline-block;
-      vertical-align: middle;
-      max-height: 100%;
-      max-width: 100%;
-    }
-  </style>
-</head>
-<body>
-  <h2>ESP32 Thermal Camera</h2>
-  <p>
-    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
-    <span class="dht-labels">Temperature: </span> 
-    <span id="temperature">%TEMPERATURE%</span>
-    <sup class="units">&deg;C</sup><br>
-    
-    <span class="dht-labels">Temp Max: </span> 
-    <span id="tempmax">%TEMPMAX%</span>
-    <sup class="units">&deg;C</sup><br>
-    
-    <span class="dht-labels">Temp Min: </span> 
-    <span id="tempmin">%TEMPMIN%</span>
-    <sup class="units">&deg;C</sup>
-
-    <img src="thermal" id="thermalimage" style="width: 100%">
-  </p>
-</body>
-<script>
-
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("temperature").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/temperature", true);
-  xhttp.send();
-}, 1000 ) ;
-
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("tempmax").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/tempmax", true);
-  xhttp.send();
-}, 1000 ) ;
-
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("tempmin").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/tempmin", true);
-  xhttp.send();
-}, 1000 ) ;
-
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    //if (this.readyState == 4 && this.status == 200) {
-      var image = document.getElementById("thermalimage");
-      //image.src = this.response;
-      image.src = "thermal";
-      //document.getElementById("thermal") = this.response;
-    //}
-  };
-  xhttp.open("GET", "/thermal", true);
-  xhttp.send();
-}, 1000 ) ;
-
-</script>
-</html>)rawliteral";
+)rawliteral";
 
 
 // Replaces placeholder with values
@@ -371,6 +316,7 @@ void lcdThermalImage(float mlx90640To[], float MinTemp, float MaxTemp)
   uint8_t w,h;
   uint8_t box = 2;
   display.setAddrWindow(0, 0, 96, 64);
+
   
   for (h = 0; h < 24; h++) {
     for (w = 0; w < 32; w++) {
@@ -414,7 +360,30 @@ void MLX_to_Serial(float mlx90640To[])
     Serial.println();
   }
 }
+  char Buf[8192];
+//String MLX_to_String(float mlx90640To[])
+char * MLX_to_Char()
+{
 
+  String framedata;
+  framedata = "{ \"data\": [ ";
+  for (int x = 0 ; x < 768 ; x++)
+  {
+    if (x!=0 ) framedata += ", ";
+    framedata += String(mlx90640To[x]);
+    
+    //Serial.print("Pixel ");
+//    Serial.print(x);
+ //   Serial.print(": ");
+  //  Serial.print(mlx90640To[x], 2);
+    //Serial.print("C");
+  //  Serial.println();
+  }
+framedata += " ] }";
+  
+  framedata.toCharArray(Buf, 8192);
+  return Buf;
+}
 
 // SETUP
 //==========================================================================
@@ -430,11 +399,71 @@ void setup()
       return;
 }
   //ESP32 As access point
-  WiFi.mode(WIFI_AP); //Access Point mode
-  WiFi.softAP(ssid, password);
+  //WiFi.mode(WIFI_AP); //Access Point mode
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAP(apssid, appassword);
+
+
+
+//char ssid = readFile(SPIFFS, "/store/sta_ssid");
+//char password = readFile(SPIFFS, "/store/sta_password");
+
+
+char ssid [512] = {'\0'};  // 511 chars and the end terminator if needed make larger/smaller
+
+File file = SPIFFS.open("/store/sta_ssid");
+  if(!file){
+      Serial.println("Failed to open file for reading");
+      return;
+  }
+  uint16_t i = 0;
+  while(file.available()){
+     ssid [i] = file.read();
+     // Serial.print (VALUE [i]); //use for debug
+     i++;
+  }
+  ssid [i] ='\0';
+  Serial.print (ssid); //use for debug
+  file.close();
+
+char password [512] = {'\0'};  // 511 chars and the end terminator if needed make larger/smaller
+  file = SPIFFS.open("/store/sta_password");
+  if(!file){
+      Serial.println("Failed to open file for reading");
+      return;
+  }
+ i = 0;
+  while(file.available()){
+     password [i] = file.read();
+     // Serial.print (VALUE [i]); //use for debug
+     i++;
+  }
+  password [i] ='\0';
+  Serial.print (password); //use for debug
+  file.close();
   
+  
+  WiFi.begin(ssid, password);
+  
+  //while (WiFi.status() != WL_CONNECTED) {
+  //  delay(500);
+  //  Serial.println("Connecting to WiFi..");
+//}
+Serial.print("ESP32 IP as soft AP: ");
+Serial.println(WiFi.softAPIP());
+Serial.println(WiFi.softAPNetworkID());
+
+
+
+/*
+  Serial.print("ESP32 IP on the WiFi network: ");
+Serial.println(WiFi.localIP());
+*/
+
   Serial.print("SDA pin: "); Serial.println(SDA);
   Serial.print("SCL pin: ");Serial.println(SCL);
+  Serial.print("SSID: "); Serial.println(apssid);
+  Serial.print("Password: ");Serial.println(appassword);
   
   Serial.println("MLX90640 IR Array Example");
 
@@ -465,10 +494,13 @@ void setup()
 // --- Part Display OLED --- //
 
   display.begin();
-
+  //display.setRotation( 1 ); // landscape
+  //display.setRotation( 0 ); // flipped landscape
   Serial.println("init");
   uint16_t time = millis();
   display.fillScreen(BLACK);
+  display.setTextSize(1);
+  
   time = millis() - time;
   Serial.println(time, DEC);
   delay(500);
@@ -491,29 +523,243 @@ void setup()
   Serial.println(ServerIP);   //IP address assigned to your ESP
   display.setCursor(0,49);
   display.print(ServerIP);    // IP address on Display
+  display.setCursor(0,59);
+
+  display.print(WiFi.localIP());
+
+
+              deleteFile(SPIFFS, "/html/cgi/frame.json");
+              deleteFile(SPIFFS, "/html/cgi/info");
+              deleteFile(SPIFFS, "/html/cgi/settings");
+  
 //----------------------------------------------------------------
 
   // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
+  //server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  //  request->send_P(200, "text/html", index_html, processor);
     //request->send_P(200, "text/html", index_html);
-  });
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+//  });
+  server.on("/cgi/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", getCenterTemp().c_str());
   });
-  server.on("/tempmax", HTTP_GET, [](AsyncWebServerRequest *request){
+  
+  server.on("/cgi/tempmax", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", getMaxTemp().c_str());
   });
-  server.on("/tempmin", HTTP_GET, [](AsyncWebServerRequest *request){
+  
+  server.on("/cgi/tempmin", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", getMinTemp().c_str());
   });
+
+  server.on("/cgi/settings", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", "Settings");
+  });
+
+  server.on("/cgi/info", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", "Info");
+  });
+  
+//server.serveStatic("/spiffs/", SPIFFS, "/");
+server.serveStatic("/", SPIFFS, "/html/");
+
+  /*
   server.on("/thermal", HTTP_GET, [](AsyncWebServerRequest *request){
+        MaxTemp = mlx90640To[0];            // Get first data to find Max and Min Temperature
+    MinTemp = mlx90640To[0];
+    
+    for (int x = 0 ; x < 768 ; x++)     // Find Maximum and Minimum Temperature
+    {
+      if (mlx90640To[x] > MaxTemp){
+        MaxTemp = mlx90640To[x];
+      }
+      if (mlx90640To[x] < MinTemp){
+        MinTemp = mlx90640To[x];
+      }
+    }
+
+
+    ThermalImageToWeb(mlx90640To, MinTemp, MaxTemp);
     request->send(SPIFFS, "/thermal.bmp", "image/bmp", false);
   });
+*/
+  
+  /*server.on("/setup.html", HTTP_GET, [](AsyncWebServerRequest *request){
+   request->send(SPIFFS, "/setup.html", "text/plain", false);
+  });
+*/
+  server.on("/cgi/save.get", HTTP_GET, [](AsyncWebServerRequest *request){
+int paramsNr = request->params();
+    Serial.println(paramsNr);
  
+    for(int i=0;i<paramsNr;i++){
+ 
+        AsyncWebParameter* p = request->getParam(i);
+        Serial.print("Param name: ");
+        Serial.println(p->name());
+        Serial.print("Param value: ");
+        Serial.println(p->value());
+        Serial.println("------");
+    }
+ 
+    request->send(200, "text/plain", "message received");
+      });
+
+  server.on(
+    "/cgi/save.post",
+    HTTP_POST,
+    [](AsyncWebServerRequest * request){},
+    NULL,
+    [](AsyncWebServerRequest * request, uint8_t *data, size_t len, size_t index, size_t total) {
+      Serial.println("SAVE POST");
+      for (size_t i = 0; i < len; i++) {
+        Serial.write(data[i]);
+      }
+ 
+      Serial.println();
+    request->send(200, "text/plain", "message received");
+  });
+
+    // Send a POST request to <IP>/post with a form field message set to <message>
+    
+    server.on("/cgi/save", HTTP_POST, [](AsyncWebServerRequest *request){
+  
+      int paramsNr = request->params();
+      Serial.println(paramsNr);
+      for(int i=0;i<paramsNr;i++){
+ 
+        AsyncWebParameter* p = request->getParam(i);
+        Serial.print("Param name: ");
+        Serial.println(p->name());
+        Serial.print("Param value: ");
+        Serial.println(p->value());
+        Serial.println("------");
+    }
+
+       
+        if (request->hasParam("sta_ssid", true)) {
+        
+            Serial.println("Found sta_ssid");
+              deleteFile(SPIFFS, "/store/sta_ssid");
+              listDir(SPIFFS, "/", 0);
+              writeFile(SPIFFS, "/store/sta_ssid", request->getParam("sta_ssid", true)->value() );
+              //writeFile(SPIFFS, "/sta_ssid", "stationssid" );
+        }
+
+            if (request->hasParam("sta_password", true)) {
+        
+            Serial.println("Found sta_password");
+              deleteFile(SPIFFS, "/store/sta_password");
+              listDir(SPIFFS, "/", 0);
+              writeFile(SPIFFS, "/store/sta_password", request->getParam("sta_password", true)->value() );
+              //writeFile(SPIFFS, "/sta_password", "password" );
+        }
+
+    
+        request->send(200, "text/plain", "message received");
+    });
+
+
+  server.on("/cgi/stassid", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Read sta_ssid");
+        listDir(SPIFFS, "/", 0);
+            
+        readFile(SPIFFS, "/store/sta_ssid");
+   request->send(SPIFFS, "/store/sta_ssid", "text/plain", false);
+  });
+
+    server.onNotFound(notFound);
+/*   server.on("/index.html", HTTP_GET, [](AsyncWebServerRequest *request){
+   request->send(SPIFFS, "/index.html", "text/plain", false);
+  });
+  */
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+   request->send(SPIFFS, "/html/index.html", "text/html", false);
+  });
+  
+  server.on("/cgi/frame.json", HTTP_GET, [](AsyncWebServerRequest *request){
+   // while(dataready == 0) {
+      
+   // }
+    request->send_P(200, "application/json", MLX_to_Char (  ) );
+ //request->send_P(200, "text/plain", MLX_to_String( mlx90640To ) );
+  //  request->send_P(200, "text/plain", "framedata" );
+  });
   server.begin();                  //Start server
   Serial.println("HTTP server started");
 }
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\r\n", path);
+    if(fs.remove(path)){
+        Serial.println("- file deleted");
+    } else {
+        Serial.println("- delete failed");
+    }
+}
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\r\n", path);
+
+    File file = fs.open(path);
+    if(!file || file.isDirectory()){
+        Serial.println("- failed to open file for reading");
+        return;
+    }
+
+    Serial.println("- read from file:");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+    file.close();
+}
+
+//void writeFile(fs::FS &fs, const char * path, const char * message){
+void writeFile(fs::FS &fs, const char * path, String message){
+    Serial.printf("Writing file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("- failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("- file written");
+    } else {
+        Serial.println("- write failed");
+    }
+    file.close();
+}
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\r\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("- failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println(" - not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("\tSIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+
 
 
 // LOOP
@@ -522,6 +768,7 @@ void setup()
 void loop()
 {
   // Read Thermal Image from MLX90640
+  dataready = 0;
   for (byte x = 0 ; x < 2 ; x++) //Read both subpages
   {
     uint16_t mlx90640Frame[834];
@@ -540,6 +787,7 @@ void loop()
 
     MLX90640_CalculateTo(mlx90640Frame, &mlx90640, emissivity, tr, mlx90640To);
   }
+  dataready = 1;
 
   // --- START of Calculate Chess Mode --- //
     // Calculate difference between Subpages (chess-mode)
@@ -608,7 +856,7 @@ void loop()
     }
 
 
-    ThermalImageToWeb(mlx90640To, MinTemp, MaxTemp);
+  //  ThermalImageToWeb(mlx90640To, MinTemp, MaxTemp);
     //display.fillRect(0, 0, 96, 48, BLACK);    // Black important sector - image and text on right side
     
     lcdThermalImage(mlx90640To, MinTemp, MaxTemp);    // Function to draw Thermal Image on OLED 
@@ -632,4 +880,3 @@ void loop()
     
   delay(100);
 }
-
